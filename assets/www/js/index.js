2a893dba2f688ce66c38b85b3997b5ef6a7bb98f
dharmendragtg <dharmendra@gotechguru.com>
2016-01-16 10:58:17 +0530

    Update index.js

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -66,7 +66,7 @@ var app = {
         window.plugins.toast.showShortCenter('PHONEGAP IS AWESOME!!!');
     },
     openWeb: function(){
-        var url = "https://www.cliqued.it/"
+        var url = "https://www.cliqued.it"
         window.open(url)
     }
 };
